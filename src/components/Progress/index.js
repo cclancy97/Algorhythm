19f8a096f3e55bcad204ef3450845ff11c9b08f4
cclancy97 <cclancy@bhcc.edu>
2019-08-28 15:29:29 -0400

    Add Music player
    installed npm package to create music player, store songs and song info
    in playlist.js

---
+++ src/components/Progress/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './index.scss'
+
+export default class Progress extends Component {
+  static propTypes = {
+    percent: PropTypes.number,
+    strokeColor: PropTypes.string,
+    strokeWidth: PropTypes.number
+  };
+
+  static defaultProps = {
+    percent: 0,
+    strokeColor: '#9b9b9b',
+    strokeWidth: 2
+  };
+
+  constructor () {
+    super()
+    this.progressContainer = React.createRef()
+  }
+
+  onClick = ({ clientX }) => {
+    const { onClick } = this.props
+    const progressRef = this.progressContainer.current
+    const progress = (clientX - progressRef.getBoundingClientRect().left) / progressRef.clientWidth
+    onClick(progress)
+  };
+
+  onKeyDown = ({ keyCode }) => {
+    const { percent, onClick } = this.props
+    switch (keyCode) {
+    case 37:
+    case 40:
+      onClick(Math.max(percent - 0.05, 0))
+      break
+    case 38:
+    case 39:
+      onClick(Math.min(percent + 0.05, 0.9999))
+      break
+    default:
+      break
+    }
+  };
+
+  render () {
+    const { percent, strokeColor, strokeWidth } = this.props
+    return (
+      <div
+        ref={this.progressContainer}
+        role="progressbar"
+        tabIndex="-1"
+        className="progress"
+        style={{ height: `${strokeWidth}px` }}
+        onClick={this.onClick}
+        onKeyDown={this.onKeyDown}
+      >
+        <div className="progress-inner" style={{ width: `${percent * 100}%`, backgroundColor: strokeColor }} />
+      </div>
+    )
+  }
+}
---
+++ src/components/playlist.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    url: 'https://res.cloudinary.com/algorhythm/video/upload/v1567012244/Queen_-_We_Will_Rock_You_tngiyh.mp3',
+    cover: 'https://res.cloudinary.com/algorhythm/image/upload/v1567012355/queen-1507300904-640x596_jzmlep.jpg',
+    title: 'We Will Rock You',
+    artist: ['Queen']
+  },
+  {
+    url: 'http://res.cloudinary.com/alick/video/upload/v1502375674/Bedtime_Stories.mp3',
+    cover: 'http://res.cloudinary.com/alick/image/upload/v1502375978/bedtime_stories_bywggz.jpg',
+    title: 'Bedtime Stories',
+    artist: ['Jay Chou']
+  },
+  {
+    url: 'http://res.cloudinary.com/alick/video/upload/v1502444212/Actor_ud8ccw.mp3',
+    cover: 'http://res.cloudinary.com/alick/image/upload/v1502444304/actor_umzdur.jpg',
+    title: '演员',
+    artist: ['薛之谦']
+  },
+  {
+    url: 'http://res.cloudinary.com/alick/video/upload/v1502444215/Bridge_of_Fate_aaksg1.mp3',
+    cover: 'http://res.cloudinary.com/alick/image/upload/v1502444306/Bridge_of_Fate_o36rem.jpg',
+    title: 'Bridge of Fate',
+    artist: ['王力宏', '谭维维']
+  },
+  {
+    url: 'http://res.cloudinary.com/alick/video/upload/v1502444222/Goodbye_byaom5.mp3',
+    cover: 'http://res.cloudinary.com/alick/image/upload/v1502444310/Goodbye_hpubmk.jpg',
+    title: 'Goodbye',
+    artist: ['G.E.M.']
+  }
+]
